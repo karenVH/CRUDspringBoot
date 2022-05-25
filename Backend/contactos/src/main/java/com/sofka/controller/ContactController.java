5efeb617591975a1265e412bd672d8f0b78e51d0
karenVH <38914172+karenVH@users.noreply.github.com>
2022-05-24 22:46:15 -0500

    actualizado  crud spring boot

--- Backend/contactos/src/main/java/com/sofka/controller/ContactController.java
+++ Backend/contactos/src/main/java/com/sofka/controller/ContactController.java
@@ -1,6 +1,6 @@
 package com.sofka.controller;
 
-import java.util.regex.Pattern;
+//import java.util.regex.Pattern;
 
 import com.sofka.domain.Contact;
 import com.sofka.service.ContactService;
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Slf4j
